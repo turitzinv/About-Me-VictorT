db66cba2f66dc5e9461479091cf94876b3fb5cc2
Victor Turitzin <turitzinv@gmail.com>
2023-07-16 14:57:24 -0400

    Added an Images folder with a Park.jpg which was imported into the Hobbies component and displayed with an img tag.

--- client/src/components/Hobbies.js
+++ client/src/components/Hobbies.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ParkImage from "../Images/Park.jpg"
 
 const Hobbies = () => {
   return (
     <div>
       <h1>Hobbies</h1>
+      <img src = {ParkImage} height="500" width="400" alt="Park" />
     </div>
   )
 }

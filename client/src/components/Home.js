5d28fb1b0ab2e4a4ec1f184cb5e0132ba69f0c6d
Victor Turitzin <turitzinv@gmail.com>
2023-07-16 15:47:46 -0400

    Added copyright message in Home screen.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
       <h1>About Me - Victor Turitzin</h1>
-     <p>Please use the navigation bar on the left to learn about me and my projects</p>
+      <p>
+        Please use the navigation bar on the left to learn about me and my
+        projects
+      </p>
+      <p>Copyright © 2023 Victor Turitzin. All rights reserved</p>
+      <p>
+        This website and its contents are protected by copyright law. Any
+        unauthorized use of this website or its contents may be a violation of
+        copyright law.
+      </p>
+      <p>
+        You may not reproduce, distribute, modify, transmit, or create
+        derivative works from this website or its contents without the prior
+        written permission of Victor Turitzin
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

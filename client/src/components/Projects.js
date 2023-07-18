a0faf98114be53a42a177aafd5148d5cb4b03c7e
Victor Turitzin <turitzinv@gmail.com>
2023-07-17 21:08:34 -0400

    Added h2 and h3 tags in Projects component.

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -5,6 +5,8 @@ const Projects = () => {
     <div>
       <h1>Projects</h1>
       <p>I've worked on a variety of projects during my time at Flatiron, some of which include areas of personal interest.</p>
+      <h2>Capstone Project - Family Recipes</h2>
+      <h3>Ruby on Rails and React Mix</h3>
     </div>
   )
 }

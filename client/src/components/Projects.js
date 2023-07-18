4b962f06a6ea0a0bbfd01a1bd6867dcdab8edd09
Victor Turitzin <turitzinv@gmail.com>
2023-07-17 21:07:16 -0400

    Added p tag description to Projects component.

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -4,6 +4,7 @@ const Projects = () => {
   return (
     <div>
       <h1>Projects</h1>
+      <p>I've worked on a variety of projects during my time at Flatiron, some of which include areas of personal interest.</p>
     </div>
   )
 }

772f1f614af5019e7c150024276539a6ac463674
Victor Turitzin <turitzinv@gmail.com>
2023-07-17 21:06:07 -0400

    Updated hobbies p tag and added two more Images in the Image folder and img tags within Hobbies component.

--- client/src/components/Hobbies.js
+++ client/src/components/Hobbies.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ParkImage from "../Images/Park.jpg"
+import DeerImage from "../Images/Deer.jpg"
+import Deer2Image from "../Images/Deer2.jpg"
 
 const Hobbies = () => {
   return (
     <div>
       <h1>Hobbies</h1>
       <img src = {ParkImage} height="500" width="400" alt="Park" />
-      <p>Add a description here about enjoying running</p>
+      <p>In my spare time I love to go for a run at my local park.</p>
+      <img src = {DeerImage} height="500" width="400" alt="Park" />
+      <img src = {Deer2Image} height="500" width="400" alt="Park" />
     </div>
   )
 }

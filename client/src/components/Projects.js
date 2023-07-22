ad1416dd03bef26cc92c2b9a1ba231667346046a
Victor Turitzin <turitzinv@gmail.com>
2023-07-22 11:46:17 -0400

    Added a p tag description describing how my capstone project idea came to be. Added css to p tag with className projects-p.

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
     <div>
       <h1>Projects</h1>
-      <p>I've worked on a variety of projects during my time at Flatiron, some of which include areas of personal interest.</p>
+      <p>
+        I've worked on a variety of projects during my time at Flatiron, some of
+        which include areas of personal interest.
+      </p>
       <h2>Capstone Project - Family Recipes</h2>
+      <p className="projects-p">
+        I had the idea to have a place to store family recipes and share them
+        with the world. I was helping family clean out old items and I came
+        across a recipe book. It was written with a pencil and the lead was
+        fading. I thought to myself that a family passdown like this should be
+        preserved.
+      </p>
       <h3>Ruby on Rails and React Mix</h3>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;

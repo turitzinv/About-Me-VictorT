9d36dfe37c411df569715bdc6e65d9d883e2ab47
Victor Turitzin <turitzinv@gmail.com>
2023-09-01 09:41:16 -0400

    Added three p tags to CodingBackground component describing my journey.

--- client/src/components/CodingBackground.js
+++ client/src/components/CodingBackground.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const CodingBackground = () => {
   return (
     <div>
       <h1>My Coding Background</h1>
       <h3>What began my interests</h3>
+      <p>
+        My coding journey began out of pure curiousity. With extra time on my
+        hands working from home during the Covid shutdown. I began watching many
+        YouTube tutorials and following along in Xcode using Swift.
+      </p>
+      <p>
+        It didn't take long for me to sign up at FreeCodeCamp and I began to
+        learn JavaScript tutorials. A life event really made me contemplate my
+        future and the career path I was following. I currently work in the
+        Advertising/Sales industry but I was curious about changing careers. Can
+        it be done? How often have people done it?
+      </p>
+      <p>
+        In my research, I came across coding bootcamps.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default CodingBackground
+export default CodingBackground;

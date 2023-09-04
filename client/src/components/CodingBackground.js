ae1de51de4cc44a707938b78895999fa5a736e56
Victor Turitzin <turitzinv@gmail.com>
2023-09-04 13:05:52 -0400

    Added css to p tags in CodingBackground component.

--- client/src/components/CodingBackground.js
+++ client/src/components/CodingBackground.js
@@ -5,19 +5,19 @@ const CodingBackground = () => {
     <div>
       <h1>My Coding Background</h1>
       <h3>What began my interests</h3>
-      <p>
+      <p className="background-p">
         My coding journey began out of pure curiousity. With extra time on my
         hands working from home during the Covid shutdown. I began watching many
         YouTube tutorials and following along in Xcode using Swift.
       </p>
-      <p>
+      <p className="background-p">
         It didn't take long for me to sign up at FreeCodeCamp and I began to
         learn JavaScript tutorials. A life event really made me contemplate my
         future and the career path I was following. I currently work in the
         Advertising/Sales industry but I was curious about changing careers. Can
         it be done? How often have people done it?
       </p>
-      <p>
+      <p className="background-p">
         In my research, I came across coding bootcamps. I was very much hesitant
         about the idea of school, especially while holding down a full time job.
         Although hesistant, the idea of it was a planted seed that began to
